f7f623d710f1a1abe1b8e46f3208035c4925c1ac
HaloHan2017 <44840315+HaloHan2017@users.noreply.github.com>
2020-07-02 15:42:41 +0800

    Update dml.sql

--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
 -- Insert record
-
+insert into student values('001','Colin',18,'male');
 -- Revise record
-
+update student set age = 20 where name = 'Colin';
 --  Delete record
-
+delete from student where name = 'Colin';
 -- Search record
+select name,age from student where id = '001';
